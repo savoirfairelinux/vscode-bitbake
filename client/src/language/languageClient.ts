8a7df7b905aeaeb0b11c38fa82edc24b3aad6870
Ziwei Wang <ziweiwzw217@gmail.com>
2023-11-03 14:04:59 -0400

    Chore: Provide extension path in initialization options

--- client/src/language/languageClient.ts
+++ client/src/language/languageClient.ts
@@ -66,7 +66,8 @@ export async function activateLanguageServer (context: ExtensionContext): Promis
       ]
     },
     initializationOptions: {
-      storagePath: context.storageUri?.fsPath
+      storagePath: context.storageUri?.fsPath,
+      extensionPath: context.extensionPath
     },
     middleware: {
       provideCompletionItem: middlewareProvideCompletion,
